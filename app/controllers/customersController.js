4ba2421e023c3edba3c58b495c29b61bde27ff64
Matt Linaberry <mtlinaberry@gmail.com>
2015-12-19 18:13:14 -0500

    Done with section 5.

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,13 +1,16 @@
 (function() {
     
-    var CustomersController = function ($scope, customersFactory, appSettings) {
+    var CustomersController = function ($scope, $log, customersFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
         
         $scope.customers = [];
         $scope.appSettings = appSettings;
         function init() {
-            $scope.customers = customersFactory.getCustomers();
+            customersFactory.getCustomers()
+            .success(function(customers) {
+                $scope.customers = customers;
+            });
         }
         init(); /// this will be called first!
         $scope.doSort = function(propName) {
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 //});
 
 //Express 4
-app.use(express.static(__dirname, '/'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/customers/:id', function(req, res) {
     var customerId = parseInt(req.params.id);
@@ -22,8 +22,8 @@ app.get('/customers/:id', function(req, res) {
 });
 
 app.get('/customers', function(req, res) {
-    res.json(customers);
-    //res.json(500, { error: 'An error has occurred!' });
+    //res.json(customers);
+    res.json(500, { error: 'An error has occurred!' });
 });
 
 app.get('/orders', function(req, res) {
@@ -54,5 +54,69 @@ app.delete('/customers/:id', function(req, res) {
 app.listen(8080);
 
 console.log('Express listening on port 8080');
+
+      var customers = [
+            {
+                id: 1, 
+                joined: '2000-12-02', 
+                name:'John', 
+                city:'Chandler', 
+                orderTotal: 9.9956,
+                orders: [
+                    {
+                        id: 1,
+                        product: 'Shoes',
+                        total: 9.9956
+                    }
+                ]
+            }, 
+            {
+                id: 2, 
+                joined: '1965-01-25',
+                name:'Zed', 
+                city:'Las Vegas', 
+                orderTotal: 19.99,
+                orders: [
+                    {
+                        id: 2,
+                        product: 'Baseball',
+                        total: 9.995
+                    },
+                    {
+                        id: 3,
+                        product: 'Bat',
+                        total: 9.995
+                    }
+                ]
+            },
+            {
+                id: 3, 
+                joined: '1944-06-15',
+                name:'Tina', 
+                city:'New York', 
+                orderTotal:44.99,
+                orders: [
+                    {
+                        id: 4,
+                        product: 'Headphones',
+                        total: 44.99
+                    }
+                ]
+            }, 
+            {
+                id: 4, 
+                joined: '1995-03-28',
+                name:'Dave', 
+                city:'Seattle', 
+                orderTotal:101.50,
+                orders: [
+                    {
+                        id: 5,
+                        product: 'Kindle',
+                        total: 101.50
+                    }
+                ]
+            }
+        ]; 
 
         
